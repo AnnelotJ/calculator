40d540d4f83ddafc545d433b0eebae9ac1541949
annelotjanssen <janssenannelot@gmail.com>
2026-06-23 20:19:58 +0200

    update

--- main.cpp
+++ main.cpp
@@ -3,22 +3,26 @@
 #include <string.h>
 #include <stack>
 #include <queue>
-#include <unordered_map>
+#include <unordered_map>    
+#include <string>
 
+
+// Stack is where all the operators are stored 
+// queue is where all the numbers are stored
 
 std::stack<char> operatorStack;
 std::queue<int> inputEnqueue;
 std::unordered_map<char, int> operatorValuesMap =
     {
-        {'*', 42},
-        {'+', 43},
-        {'-', 45},
-        {'/', 47}
+        {'*', 2},
+        {'+', 1},
+        {'-', 1},
+        {'/', 2}
     };
 
 
 void pushOperatorsToStack (char);
-void pushNumbersToEnqueue (char); 
+void pushNumbersToEnqueue (int); 
 bool isHighPrecedence(char, char);
 
 int main (){
@@ -28,74 +32,100 @@ int main (){
     std::getline(std::cin,inputStringCalculation);
     
     for (int i=0; i <inputStringCalculation.length(); i++){
-
+        
         if (inputStringCalculation[i] == '+' || inputStringCalculation[i] == '-' || inputStringCalculation[i] == '/' || inputStringCalculation[i] == '*' ){
-            // Look at what the current stack is. 
+            
             if (operatorStack.size() == 0){
                 operatorStack.push(inputStringCalculation[i]);
             }
-            else {
+            else if (operatorStack.size()>0){
+                std::cout<<"2345678";
                 if( operatorStack.top() == '+'){
-                    if (isHighPrecedence(operatorStack.top(), inputStringCalculation[i]) == true){
-                        inputEnqueue.push(operatorStack.top()); 
-                        operatorStack.pop();
-                        operatorStack.push(inputStringCalculation[i]);
-                    }
-                    else {
-                        operatorStack.push(inputStringCalculation[i]);
-                    }
-                } 
-                if( operatorStack.top() == '-'){
-                    std::cout << "hello I <3 you";
-                    if(isHighPrecedence(operatorStack.top(), inputStringCalculation[i]) == true){
-                         std::cout << "\n";
-                        std::cout << operatorStack.top();
-                        inputEnqueue.push(operatorStack.top());
-                        operatorStack.pop(); 
-                         std::cout << "\n";
-                        std::cout << inputStringCalculation;
-                        operatorStack.push(inputStringCalculation[i]);
-                        std::cout << operatorStack.top(); 
-                        std::cout << "\n";
-                        
-                    }
-                    else{
-                        operatorStack.push(inputStringCalculation[i]);
-                    }
+                        if (isHighPrecedence(operatorStack.top(), inputStringCalculation[i]) == true){
+                            std::cout<<"dbsfjhsfjbs";
+                            inputEnqueue.push(operatorStack.top()); 
+                            operatorStack.pop();
+                            operatorStack.push(inputStringCalculation[i]);
+                        }
+                        else {
+                            operatorStack.push(inputStringCalculation[i]);
+                        }
+                    } 
+                else if( operatorStack.top() == '-'){
+                    std::cout<<"swh3194bsdf";
+                        if(isHighPrecedence(operatorStack.top(), inputStringCalculation[i]) == true){
+                            inputEnqueue.push(operatorStack.top());
+                            operatorStack.pop(); 
+                            operatorStack.push(inputStringCalculation[i]);                     
+                        }
+                        else{
+                            operatorStack.push(inputStringCalculation[i]);
+                        }
 
-                } 
-                if( operatorStack.top() == '*'){
-                    if (isHighPrecedence(operatorStack.top(), inputStringCalculation[i]) == true ){
-                        inputEnqueue.push(operatorStack.top()); 
-                        operatorStack.pop();
-                        operatorStack.push(inputStringCalculation[i]);
-                    }
-                    else{
-                        operatorStack.push(inputStringCalculation[i]);
-                    }
+                    } 
+                else if( operatorStack.top() == '*'){
+                        if (isHighPrecedence(operatorStack.top(), inputStringCalculation[i]) == true ){
+                            inputEnqueue.push(operatorStack.top()); 
+                            operatorStack.pop();
+                            operatorStack.push(inputStringCalculation[i]);
+                        }
+                        else{
+                            operatorStack.push(inputStringCalculation[i]);
+                        }
 
-                } 
-                if( operatorStack.top() == '/'){
-                    if (isHighPrecedence(operatorStack.top(), inputStringCalculation[i]) == true ){ 
-                        inputEnqueue.push(operatorStack.top()); 
-                        operatorStack.pop();
-                        operatorStack.push(inputStringCalculation[i]);
-                    }
-                    else {
-                        operatorStack.push(inputStringCalculation[i]);
-                    }
-                } 
+                    } 
+                else if( operatorStack.top() == '/'){
+                        if (isHighPrecedence(operatorStack.top(), inputStringCalculation[i]) == true ){ 
+                            inputEnqueue.push(operatorStack.top()); 
+                            operatorStack.pop();
+                            operatorStack.push(inputStringCalculation[i]);
+                        }
+                        else {
+                            operatorStack.push(inputStringCalculation[i]);
+                        }
+                    } 
+                
+                    // I want to see what the current stack is and how it is organised
+                    // std::cout<<"the stack is as follows:\n";
+                    // std::stack<char> copeOperatorStack = operatorStack; 
+                    // while (!copeOperatorStack.empty()){
+                    //     std::cout<< copeOperatorStack.top() << "\n"; 
+                    //     copeOperatorStack.pop();
+                    // }
+                    // std::cout<<"'\n";
             }
         }
-        pushNumbersToEnqueue(i);
+        else if (inputStringCalculation[i] >= '0' && inputStringCalculation[i]<='9'){
+            pushNumbersToEnqueue(inputStringCalculation[i]-'0');
+            
+        }
+        //  std::cout<<"--------------------------------------------";
+        //     std::cout << "\nThe queue is as followed: \n";
+        //     std::queue<int> copy = inputEnqueue;
+        //     while (!copy.empty()) {
+        //         std::cout << copy.front() << " ";
+        //         copy.pop();
+        //     }
+        //     std::cout << "\n";
+
+        // // I want to see what the current stack is and how it is organised
+        //         std::cout<<"the stack is as follows:\n";
+        //         std::stack<char> copeOperatorStack = operatorStack; 
+        //         while (!copeOperatorStack.empty()){
+        //             std::cout<< copeOperatorStack.top() << " "; 
+        //             copeOperatorStack.pop();
+        //         }
+        //         std::cout<<"\n";
+        //         std::cout<<"------------------------------------------\n";
     }
+            std::cout<<"......................................................";
+            std::cout << "\nThe final queue is as followed: \n";
             std::queue<int> copy = inputEnqueue;
             while (!copy.empty()) {
                 std::cout << copy.front() << " ";
                 copy.pop();
             }
             std::cout << "\n";
-            
 
     return 0;
 }
@@ -104,7 +134,6 @@ int main (){
 bool isHighPrecedence (char operatorFromStack,char currentOperator){
 
     if ( operatorValuesMap[operatorFromStack] > operatorValuesMap[currentOperator]){
-        // Stack is higher than current 
         return true;
     }
     else {
@@ -118,6 +147,6 @@ void pushOperatorsToStack (char inputOperators){
 
 }
 
-void pushNumbersToEnqueue (char inputNumber){
+void pushNumbersToEnqueue (int inputNumber){
     inputEnqueue.push(inputNumber);
 }
